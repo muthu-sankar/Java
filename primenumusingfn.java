5be098e1158b6b6b4c0356706a4000fdbddb51a1
muthu-sankar <smuthusankar7@gmail.com>
2024-02-06 17:32:29 +0530

    feat :"Finding Prime number using Function"

--- primenumusingfn.java
+++ primenumusingfn.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-public class primenumudingfn {
+public class primenumusingfn {
+
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
         System.out.print("Enter the number to check Prime\t");
